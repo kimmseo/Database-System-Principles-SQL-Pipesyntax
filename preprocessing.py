4cf0ad0c3ce7c0a5c507b8e2b3b699f3bcc346eb
Iqoyz <Chanyonqi@hotmail.com>
2025-04-01 12:19:45 +0800

    chore: enable manual input for db connection

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,8 @@
 import psycopg2
 import json
 
-def get_qep(sql_query):
-    conn = psycopg2.connect(
-        # Edit to personal database details
-        dbname="TPC-H", user="postgres", password="password", host="localhost", port="5432"
-    )
+def get_qep(sql_query, db_config):
+    conn = psycopg2.connect(**db_config)
     cur = conn.cursor()
     cur.execute(f"EXPLAIN (FORMAT JSON) {sql_query}")
     result = cur.fetchone()[0]
@@ -13,5 +10,5 @@ def get_qep(sql_query):
     conn.close()
     # Debug
     # Print the full QEP
-    print(json.dumps(result[0], indent=2))
+    # print(json.dumps(result[0], indent=2))
     return result[0]  # unwrap from list
